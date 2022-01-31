12ad9e57d3c287de34f0eb502f8d426a2b7f7238
Henry Qin <root@hq6.me>
2022-01-30 17:29:54 -0800

    WIP: Working E2E with a simple hello world greeter.
    Next steps.
    2. Look in protobuf library to see if there's a topological sort for
       file descriptors that is built in so we can get rid of the
       descriptor_database jankiness and go back to using
       DescriptorPool.BuildFile.
    3. Clean up the TODOs.
    4. Fix the imports to respect the protobuf / grpc remapping thingie.
    5. Read the contributing guidelines / style guide and fix anything
       there.
    6. Figure out why EnvoyExceptions are not printed and whether they are
       supposed to be, when called form InitManager?
    7. Write unit tests.

--- source/extensions/filters/http/grpc_json_transcoder/config.cc
+++ source/extensions/filters/http/grpc_json_transcoder/config.cc
@@ -5,6 +5,7 @@
 #include "envoy/registry/registry.h"
 
 #include "source/extensions/filters/http/grpc_json_transcoder/json_transcoder_filter.h"
+#include "source/common/common/assert.h"
 
 namespace Envoy {
 namespace Extensions {
@@ -15,9 +16,8 @@ Http::FilterFactoryCb GrpcJsonTranscoderFilterConfig::createFilterFactoryFromPro
     const envoy::extensions::filters::http::grpc_json_transcoder::v3::GrpcJsonTranscoder&
         proto_config,
     const std::string&, Server::Configuration::FactoryContext& context) {
-  JsonTranscoderConfigSharedPtr filter_config =
-      std::make_shared<JsonTranscoderConfig>(proto_config, context.api());
 
+  JsonTranscoderConfigSharedPtr filter_config = createConfig(proto_config, context);
   return [filter_config](Http::FilterChainFactoryCallbacks& callbacks) -> void {
     callbacks.addStreamFilter(std::make_shared<JsonTranscoderFilter>(*filter_config));
   };
@@ -28,8 +28,45 @@ GrpcJsonTranscoderFilterConfig::createRouteSpecificFilterConfigTyped(
     const envoy::extensions::filters::http::grpc_json_transcoder::v3::GrpcJsonTranscoder&
         proto_config,
     Server::Configuration::ServerFactoryContext& context, ProtobufMessage::ValidationVisitor&) {
+  return createConfig(proto_config, context);
+}
 
-  return std::make_shared<JsonTranscoderConfig>(proto_config, context.api());
+JsonTranscoderConfigSharedPtr GrpcJsonTranscoderFilterConfig::createConfig(
+    const envoy::extensions::filters::http::grpc_json_transcoder::v3::GrpcJsonTranscoder&
+        proto_config,
+    Server::Configuration::CommonFactoryContext& context) {
+  // Construct descriptor_pool_builder differently depending on whether we need
+  // reflection or not. The cases are spelled out mostly for readability; this
+  // could be collapsed into a single line with a ternary operator if desired.
+  std::shared_ptr<DescriptorPoolBuilder> descriptor_pool_builder;
+  switch (proto_config.descriptor_set_case()) {
+    case envoy::extensions::filters::http::grpc_json_transcoder::v3::GrpcJsonTranscoder::
+        DescriptorSetCase::kProtoDescriptor:
+    case envoy::extensions::filters::http::grpc_json_transcoder::v3::GrpcJsonTranscoder::
+        DescriptorSetCase::kProtoDescriptorBin:
+      descriptor_pool_builder = std::make_shared<DescriptorPoolBuilder>(proto_config, context.api());
+      break;
+    case envoy::extensions::filters::http::grpc_json_transcoder::v3::GrpcJsonTranscoder::
+        DescriptorSetCase::kProtoDescriptorClusterName:
+        {
+          std::shared_ptr<AsyncReflectionFetcher> async_reflection_fetcher =
+            std::make_shared<AsyncReflectionFetcher>(
+                proto_config.proto_descriptor_cluster_name(),
+                proto_config.services(),
+                context.api().timeSource(),
+                context.clusterManager(),
+                context.initManager());
+          // This needs to be injected into descriptor_pool_builder so that it can
+          // transitively stay alive long enough to do its job.
+          descriptor_pool_builder =
+              std::make_shared<DescriptorPoolBuilder>(proto_config, async_reflection_fetcher);
+          break;
+        }
+    default:
+      NOT_REACHED_GCOVR_EXCL_LINE;
+  }
+
+  return std::make_shared<JsonTranscoderConfig>(proto_config, descriptor_pool_builder);
 }
 
 /**
